4bba2f3cae83679c530df095a931fd0433e85ae4
ykara <yasinomer.kara@booking.com>
2024-11-27 19:29:20 +0300

    changes added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from user_input import ask_user_questions
 from concat_sources import concat_sources
 from filter_data import filter_excel_with_answers
+from automation import copy_paste_data
 
 def main():
     # Call the function from questionnaire.py
@@ -10,7 +11,7 @@ def main():
     if answers["files_loaded"] == "E":
         concat_sources()
         filter_excel_with_answers(answers)
-    
+        copy_paste_data(answers)
 
 if __name__ == "__main__":
     main()
